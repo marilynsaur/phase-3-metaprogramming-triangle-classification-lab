5db52474510505c2c0212e9d39cbfe460bc4279c
Marilyn Wren <Marilynwren64@yahoo.com>
2022-12-14 16:08:03 -0500

    commit tree d92b4cd000d81ddd64c5ab0050127240933dfc78

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+   
+    
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+ 
+  def kind
+   
+
+    if a == b && b == c
+     :equilateral
+    elsif a == b || b ==c || a== c
+    :isosceles
+    else
+    :scalene
+    end
+#     begin
+#      raise TriangleError
+#     rescue TriangleError => error
+#       puts error.message
+#     end
+#   end
+# end
+
+ 
+end
+
+#  class TriangleError < StandardError
+#   def message
+#     "triangles violating triangle inequality"
+#   end
+#   end
+
+
+
 end
